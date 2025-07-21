64d3b58e63f8c10fd2f9dd20214521b2cc9189f9
2024Emprendimiento <wayuunuti@gmail.com>
2025-07-21 10:40:23 -0500

    Update mail_config.php

--- config/mail_config.php
+++ config/mail_config.php
@@ -4,9 +4,9 @@ return [
     'smtp_host' => 'smtp.gmail.com',
     'smtp_port' => 587, // para TLS,
     'smtp_auth' => true,
-    'smtp_username' => '2025developertest@gmail.com', // Tu dirección de Gmail completa
-    'smtp_password' => 'ztux pilr issc xxqc',
+    'smtp_username' => '', // Tu dirección de Gmail completa
+    'smtp_password' => '',
     'smtp_encryption' => 'tls',
-    'from_address' => '2025developertest@gmail.com', // También puedes usar otra dirección si está configurada como alias
-    'from_name' => 'Astrid',
+    'from_address' => '', // También puedes usar otra dirección si está configurada como alias
+    'from_name' => '',
 ];
